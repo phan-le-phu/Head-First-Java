9765d93dde1690baccd7204e83cbe4380df1a842
phan-le-phu <18521247@gm.uit.edu.vn>
2022-01-11 10:05:40 +0700

    Do exercise1b exercise

---
+++ 1 dive in A Quick Dip/exercises/Exercise1b.java
@@ -0,0 +1,44 @@
+// A 
+/*
+class Exercise1b {
+  public static void main(String[] args) {
+    int x = 1;
+    while ( x < 10 ) {
+      if ( x >  3 ) {
+        System.out.println("big x");
+      }
+
+      x = x + 1;
+    }
+  }
+}
+*/
+
+// B
+/*
+class Exercise1b {
+  public static void main(String[] args) {
+    int x = 5;
+    while ( x > 1 ) {
+      x = x - 1;
+      if ( x < 3 ) {
+        System.out.println("small x");
+      }
+    }
+  }
+}
+*/
+
+// C 
+
+class Exercise1b {
+  public static void main(String[] args){
+    int x = 5;
+    while (x > 1) {
+      x = x - 1;
+      if (x < 3) {
+        System.out.println("small x");
+      }
+    }
+  }
+}
