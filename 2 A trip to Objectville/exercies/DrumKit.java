d478e7e425633c7768bf749a99874f1a9a97689b
phan-le-phu <18521247@gm.uit.edu.vn>
2022-01-12 10:37:14 +0700

    Do 'code magnets' exercie

---
+++ 2 A trip to Objectville/exercies/DrumKit.java
@@ -0,0 +1,29 @@
+class DrumKit {
+  boolean topHat = true;
+  boolean snare = true;
+
+  void playSnare() {
+    System.out.println("bang bang ba-bang");
+  }
+
+  void playTopHat() {
+    System.out.println("ding ding da-ding");
+  }
+}
+
+class DrumKitTestDrive {
+
+  static public void main(String[] args){
+
+    DrumKit d = new DrumKit();
+
+    d.playSnare();
+    d.playTopHat();
+
+    d.snare = false;
+
+    if (d.snare == true) {
+      d.playSnare();
+    }
+  }
+}
